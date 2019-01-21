f8a6ab0e20fa268d9ab7bf8f9199bbda6f84f2f9
root <root@ip-10-0-0-188.ec2.internal>
2019-01-21 16:12:27 +0530

    Done!!!!

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #echo
-echo "This is a Example of GIT Script"
+echo "This is a Example of GIT Commit Script"
 NUM=$(($FIRSTNO + $SECONDNO))
 echo "$NUM"
 echo "This is Done based on Committttttttttt!"
